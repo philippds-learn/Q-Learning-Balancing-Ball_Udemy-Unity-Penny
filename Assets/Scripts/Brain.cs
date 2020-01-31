bb26b90e1c31ab0bbcc7d21b969e3b546fe99d62
Philipp Siedler <Philipp.Siedler@zaha-hadid.com>
2020-01-31 16:06:49 +0000

    finishing balancing ball

--- Assets/Scripts/Brain.cs
+++ Assets/Scripts/Brain.cs
@@ -47,12 +47,149 @@ public class Brain : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.ann = new ANN(3, 2, 1, 6, 0.2f);
+        this.ballStartPos = this.ball.transform.position;
+        Time.timeScale = 5.0f;
+    }
+
+    GUIStyle guiStyle = new GUIStyle();
+    private void OnGUI()
+    {
+        this.guiStyle.fontSize = 25;
+        this.guiStyle.normal.textColor = Color.white;
+        GUI.BeginGroup(new Rect(10, 10, 600, 150));
+        GUI.Box(new Rect(0, 0, 140, 140), "Stats", this.guiStyle);
+        GUI.Label(new Rect(10, 25, 500, 30), "Fails: " + this.failCount, this.guiStyle);
+        GUI.Label(new Rect(10, 50, 500, 30), "Decay Rate: " + this.exploreRate, this.guiStyle);
+        GUI.Label(new Rect(10, 75, 500, 30), "Last Best Balance: " + this.maxBalanceTime, this.guiStyle);
+        GUI.Label(new Rect(10, 100, 500, 30), "This Balance: " + this.timer, this.guiStyle);
+        GUI.EndGroup();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown("space"))
+        {
+            ResetBall();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        this.timer += Time.deltaTime;
+        List<double> states = new List<double>();
+        List<double> qs = new List<double>();
+
+        states.Add(this.transform.rotation.x);
+        states.Add(this.ball.transform.position.z);
+        states.Add(this.ball.GetComponent<Rigidbody>().angularVelocity.x);
+
+        qs = SoftMax(this.ann.CalcOutput(states));
+        double maxQ = qs.Max();
+        int maxQIndex = qs.ToList().IndexOf(maxQ);
+        this.exploreRate = Mathf.Clamp(this.exploreRate - this.exploreDecay, this.minExploreRate, this.maxExploreRate);
+
+        /*
+        if(Random.Range(0, 100) < this.exploreRate)
+        {
+            maxQIndex = Random.Range(0, 2);
+        }
+        */
+
+        if(maxQIndex == 0)
+        {
+            this.transform.Rotate(Vector3.right, this.tiltSpeed + (float)qs[maxQIndex]);
+        }
+        else if(maxQIndex == 1)
+        {
+            this.transform.Rotate(Vector3.left, this.tiltSpeed + (float)qs[maxQIndex]);
+        }
+
+        if(this.ball.GetComponent<BallState>().dropped)
+        {
+            this.reward = -1.0f;
+        }
+        else
+        {
+            this.reward = 0.1f;
+        }
+
+        Replay lastMemory = new Replay(this.transform.rotation.x, this.ball.transform.position.z, this.ball.GetComponent<Rigidbody>().angularVelocity.x, this.reward);
+
+        if(this.replayMemory.Count > this.mCapacity)
+        {
+            this.replayMemory.RemoveAt(0);
+        }
+
+        this.replayMemory.Add(lastMemory);
+
+        if(this.ball.GetComponent<BallState>().dropped)
+        {
+            for(int i = replayMemory.Count - 1; i >= 0; i--)
+            {
+                List<double> toutputsOld = new List<double>();
+                List<double> toutputsNew = new List<double>();
+                toutputsOld = SoftMax(this.ann.CalcOutput(this.replayMemory[i].states));
+
+                double maxQOld = toutputsOld.Max();
+                int action = toutputsOld.ToList().IndexOf(maxQOld);
+
+                double feedback;
+                if(i == this.replayMemory.Count - 1 || this.replayMemory[i].reward == -1)
+                {
+                    feedback = this.replayMemory[i].reward;
+                }
+                else
+                {
+                    toutputsNew = SoftMax(this.ann.CalcOutput(this.replayMemory[i + 1].states));
+                    maxQ = toutputsNew.Max();
+                    feedback = (this.replayMemory[i].reward + this.discount * maxQ);
+                }
+
+                toutputsOld[action] = feedback;
+                this.ann.Train(this.replayMemory[i].states, toutputsOld);
+            }
+
+            if(this.timer > this.maxBalanceTime)
+            {
+                this.maxBalanceTime = this.timer;
+            }
+
+            this.timer = 0;
+
+            this.ball.GetComponent<BallState>().dropped = false;
+            this.transform.rotation = Quaternion.identity;
+            ResetBall();
+            this.replayMemory.Clear();
+            this.failCount++;
+        }
+    }
+
+    void ResetBall()
+    {
+        this.ball.transform.position = this.ballStartPos;
+        this.ball.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        this.ball.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+    }
+
+    List<double> SoftMax(List<double> values)
+    {
+        double max = values.Max();
+
+        float scale = 0.0f;
+
+        for(int i = 0; i < values.Count; ++i)
+        {
+            scale += Mathf.Exp((float)(values[i] - max));
+        }
+
+        List<double> result = new List<double>();
+        for(int i = 0; i < values.Count; ++i)
+        {
+            result.Add(Mathf.Exp((float)(values[i] - max)) / scale);
+        }
+
+        return result;
     }
 }
